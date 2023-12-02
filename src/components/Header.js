858d1259a6347a18ef9cf65877a69898564c2874
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-02 14:35:02 -0700

    Add CSS to logo and add buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,28 @@
-import './Header.css'
-import { Link } from 'react-router-dom';
+import "./Header.css";
+import { Link } from "react-router-dom";
 
-function Header () {
+function Header() {
   return (
-    <header className="header">
-       <h1 className='header-title'>AI AT Ally</h1>
-      <div className='logo-title-container'></div>
+    <header className='header'>
       <Link to='/'>
-      <img src="help.png" className="help-logo" alt="Helping Hands Logo" />
+        <div className='logo-title-container'>
+          <img src='help.png' className='help-logo' alt='Helping Hands Logo' />
+
+          <div className='title-container'>
+            <h1 className='title'>AI AT Ally</h1>
+            <p className='header-title'>
+              <em>Artificial Intelligence Assistive Tech </em>
+            </p>
+          </div>
+        </div>
       </Link>
-      <p className='header-title'>
-        <em>Artificial Intelligence Assistive Tech </em>
-      </p>
+
+      <div className='button-container'>
+        <button className='about-button'>ABOUT</button>
+        <button className='sign-in-button'>SIGN IN</button>
+      </div>
     </header>
-    
-  )
+  );
 }
 
-export default Header
+export default Header;
