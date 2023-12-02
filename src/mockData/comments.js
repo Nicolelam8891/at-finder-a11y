03cf238445a96711e9301f441a0abb589be94234
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-01 18:19:25 -0700

    Add mock comments

---
+++ src/mockData/comments.js
@@ -0,0 +1,22 @@
+const comments = [
+    {
+      title: "title 1",
+      link: "www.google.com",
+      category: "Blindness",
+      description: "string",
+      userComment: "This is my comment. I hate this technology! Booo!",
+      rating: false,
+      userRecommended: false, 
+      id: 2
+  },
+  {
+    title: "title 2",
+    link: "www.google.com",
+    category: "Blindness",
+    description: "string",
+    userComment: "This is my second comment. I love this technology! Wooo!",
+    rating: true,
+    userRecommended: true, 
+    id: 1
+},
+]
