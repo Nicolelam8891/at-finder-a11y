f5c06d6485af2ae9070506b5b550a3681a709eac
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-02 12:23:56 -0700

    Iterate over categories

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,17 @@
 import './Form.css'
 import needs from '../mockData/needs'
+import CategoryCard from './CategoryCard';
 
 function Form () {
   
   console.log(needs)
-  
+  const categories = Object.keys(needs);
+
   return (
     <div>
-
+      {categories.map(category => (
+        <CategoryCard key={category} category={category} data={needs[category]} />
+      ))}
     </div>
   )
 }
