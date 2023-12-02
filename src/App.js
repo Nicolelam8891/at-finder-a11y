0f29aa066a28821f6fdc5f14227a4c2b43a04101
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-02 16:11:20 -0700

    Add background for splash page only

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
 
 import React, { useState } from 'react';
-import Form from './Form';
+// import Form from './Form';
 import Header from './components/Header';
+import Home from './components/Home';
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedTechParam, setSelectedTechParam] = useState(null);
 
-  const handleTechParamsSelection = (category, techParam) => {
-    setSelectedCategory(category);
-    setSelectedTechParam(techParam);
-    fetch('your-api-endpoint', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        needs: {
-          [category]: {
-            tech_needs: techParam,
-            disability_category: needs[category]['disability parameter'],
-          },
-        },
-      }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  };
+  // const handleTechParamsSelection = (category, techParam) => {
+  //   setSelectedCategory(category);
+  //   setSelectedTechParam(techParam);
+  //   fetch('your-api-endpoint', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       needs: {
+  //         [category]: {
+  //           tech_needs: techParam,
+  //           disability_category: needs[category]['disability parameter'],
+  //         },
+  //       },
+  //     }),
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //     });
+  // };
 
   return (
     <main className="App">
       <Header />
+      <Home />
       <h1>Your App</h1>
-      <Form onTechParamsSelection={handleTechParamsSelection} />
+      {/* <Form onTechParamsSelection={handleTechParamsSelection} /> */}
     </main>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import './Home.css'
 
 function Home () {
   return (
-    <div className>
+    <div className='home-background'>
 
     </div>
   )
