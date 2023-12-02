e11600ca993e4776fa8fe0ef5b088664e60f046f
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-01 18:19:38 -0700

    Add mockTech

---
+++ src/mockData/mockTech.js
@@ -0,0 +1,26 @@
+const tech = [
+  {
+    type: "screen readers", 
+    title: "NVDA",
+    description: "Lorem ipsum lorem ipsum blah blah blah this is filler text",
+    urls: ["https://www.google.com", "https://www.nvaccess.org/"]
+  },
+  {
+    type: "screen readers", 
+    title: "NVDA",
+    description: "Lorem ipsum lorem ipsum blah blah blah this is filler text",
+    urls: ["https://www.google.com", "https://www.nvaccess.org/"]
+  },
+  {
+    type: "refreshable braille displays",
+    title: "JAWS",
+    description: "Hello hello hello hello hello aefjah ejfa",
+    urls: ["https://www.freedomscientific.com/products/software/jaws/"]
+  },
+  {
+    type: "refreshable braille displays",
+    title: "JAWS",
+    description: "Hello hello hello hello hello aefjah ejfa",
+    urls: ["https://www.freedomscientific.com/products/software/jaws/"]
+  }
+]
