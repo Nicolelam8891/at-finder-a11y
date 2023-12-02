f9c81dfaec11e3825a71c6d934337eb677297990
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-01 18:19:59 -0700

    Add needs data

---
+++ src/mockData/needs.js
@@ -0,0 +1,47 @@
+const needs = [
+  {
+      "category": "Blindness",
+      "technology parameter": ["screen readers", "refreshable braille displays"],
+      "disability parameter": "are totally blind and cannot use a mouse"
+  },
+  {
+      "category": "Low Vision",
+      "technology parameter": ["Magnification Software", "Built-in Browser Tools", "CCTV", "Magnifying Devices"],
+      "disability parameter": "have low vision"
+  },
+  {
+      "category": "Deaf / Hard of Hearing",
+      "technology parameter": ["enhance hearing", "telecommunication support", "sign language", "alerting device"],
+      "disability parameter": "are deaf or hard of hearing"
+  },
+  {
+      "category": "Mobility Supports for Fine Motor Issues",
+      "technology parameter": ["personal care and hygiene", "dressing and clothing", "writing and communication", "cooking and meal preparation", "home organization and accessibility", "mobility and transportation", "environmental control", "computer and internet access", "entertainment and leisure", "personal safety"],
+      "disability parameter": "have fine motor disabilities"
+  },
+  {
+      "category": "Mobility Supports for Gross Motor Issues",
+      "technology parameter": ["mobility aids", "home accessibility and environmental control", "communication aids", "adaptive computer access", "daily living aids", "vehicle modifications", "recreation and leisure", "personal safety and emergency response"],
+      "disability parameter": "have gross motor disabilities"
+  },
+  {
+      "category": "Cognitive Supports for Reading and Writing",
+      "technology parameter": ["text-to-speech", "mind mapping", "word prediction", "speech to text", "graphic organizer"],
+      "disability parameter": "have cognitive disabilities in reading and writing"
+  },
+  {
+      "category": "Cognitive Supports for Math",
+      "technology parameter": ["computation", "accessible calculators"],
+      "disability parameter": "have cognitive disabilities in math"
+  },
+  {
+      "category": "Cognitive Supports for Focus",
+      "technology parameter": ["noise-cancelling headphones", "background music", "time management", "white noise", "brain training", "note taking", "calendar supports"],
+      "disability parameter": "have difficulty maintaining cognitive focus"
+  },
+  {
+      "category": "Communication Supports",
+      "technology parameter": ["physical support", "cognitive support"],
+      "disability parameter": "have communication disabilities"
+  }
+]
