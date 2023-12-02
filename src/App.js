7cdbc83af0ea2c6068aed07a551320c7b2357d76
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-02 13:39:07 -0700

    Add header with CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { Route, Routes } from 'react-router';
 import Form from './components/Form'
+import Header from './components/Header';
 
 function App() {
+
   return (
+      <>
+      <Header />
     <main className="App">
       <Form/>
     </main>
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import './Header.css'
+import { Link } from 'react-router-dom';
 
 function Header () {
   return (
-    <div>
-
-    </div>
+    <header className="header">
+       <h1 className='header-title'>AI AT Ally</h1>
+      <div className='logo-title-container'></div>
+      <Link to='/'>
+      <img src="help.png" className="help-logo" alt="Helping Hands Logo" />
+      </Link>
+      <p className='header-title'>
+        <em>Artificial Intelligence Assistive Tech </em>
+      </p>
+    </header>
+    
   )
 }
 
