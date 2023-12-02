36265c7c399707b457b93635d71e02fc22cbe398
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-02 15:47:53 -0700

    Set up Post

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import './App.css';
-import { Route, Routes } from 'react-router';
-import Form from './components/Form'
+// App.js
+import React, { useState } from 'react';
+import Form from './Form';
 
-function App() {
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedTechParam, setSelectedTechParam] = useState(null);
+
+  const handleTechParamsSelection = (category, techParam) => {
+    setSelectedCategory(category);
+    setSelectedTechParam(techParam);
+    fetch('your-api-endpoint', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        needs: {
+          [category]: {
+            tech_needs: techParam,
+            disability_category: needs[category]['disability parameter'],
+          },
+        },
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
   return (
-    <main className="App">
-      <Form/>
-    </main>
+    <div>
+      <h1>Your App</h1>
+      <Form onTechParamsSelection={handleTechParamsSelection} />
+      {/* Other components or content */}
+    </div>
   );
-}
+};
 
 export default App;
