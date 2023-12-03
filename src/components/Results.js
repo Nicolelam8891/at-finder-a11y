4954e8c78823c13f3a05ecb5c360618609ec0421
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-03 13:42:19 -0700

    Add component js

---
+++ src/components/Results.js
@@ -0,0 +1,14 @@
+import './Results.css'
+import TechList from './TechList'
+import CommentSection from 
+
+function Results () {
+  return (
+    <div>
+      <TechList />
+      <CommentSection />
+    </div>
+  )
+}
+
+export default Results
