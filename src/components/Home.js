1bedccee03eb4c0904e2f4f3a5e326c1d3547bb0
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-02 21:03:04 -0700

    Add text box and CSS for AT

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,17 @@
-import './Home.css'
+import "./Home.css";
 
-function Home () {
+function Home() {
   return (
     <div className='home-background'>
       <div className='overlay'></div>
-     
-    
+      <div className='AT-info'>
+        <h1>What is assistive technology (AT)</h1>
+        <p>Assistive technology (AT) is any item, piece of equipment, software program, or product system that is used to increase, maintain, or improve the functional capabilities of persons with disabilities. Assistive technology helps people who have difficulty speaking, typing, writing, remembering, pointing, seeing, hearing, learning, walking, and many other things. Different disabilities require different assistive technologies. </p>
+      </div>
     </div>
-  )
+    
+    
+  );
 }
 
-export default Home
+export default Home;
