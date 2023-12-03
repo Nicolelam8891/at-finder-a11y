215322d937d6e48f1a15d75ed25440593f666b97
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-03 12:36:27 -0700

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
 
 import React, { useState } from 'react';
-import Form from './Form';
+import Form from './components/Form';
 import Header from './components/Header';
+import Home from './components/Home'
+import {Route, Routes} from 'react-router-dom'
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedTechParam, setSelectedTechParam] = useState(null);
 
-  const handleTechParamsSelection = (category, techParam) => {
-    setSelectedCategory(category);
-    setSelectedTechParam(techParam);
-    fetch('your-api-endpoint', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        needs: {
-          [category]: {
-            tech_needs: techParam,
-            disability_category: needs[category]['disability parameter'],
-          },
-        },
-      }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  };
+  // const handleTechParamsSelection = (category, techParam) => {
+  //   setSelectedCategory(category);
+  //   setSelectedTechParam(techParam);
+  //   fetch('your-api-endpoint', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       needs: {
+  //         [category]: {
+  //           tech_needs: techParam,
+  //           disability_category: needs[category]['disability parameter'],
+  //         },
+  //       },
+  //     }),
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //     });
+  // };
+
+      {/* <Form onTechParamsSelection={handleTechParamsSelection} /> */}
 
   return (
     <main className="App">
       <Header />
-      <h1>Your App</h1>
-      <Form onTechParamsSelection={handleTechParamsSelection} />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/findmyat/*' element={<Form/>}/>
+      </Routes>
     </main>
   );
 };
