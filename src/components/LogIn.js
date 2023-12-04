69c222aa01d10981c7c7085a8aaeab35fbab58a2
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-04 11:29:29 -0700

    Add a login component

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,8 +1,34 @@
 import './Login.css'
 
-function Login () {
+function Login ( {isOpen, onClose, onLogin} ) {
+  cons [username, setUserName] = useState('');
+  cons [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onLogin(username, password);
+  };
+
+  if (!isOpen) return null; 
+
   return (
-    <div>
+    <div className='login-modal'>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(event) => setUsername(e.target.value)}
+          />
+          <input 
+            type="text"
+            placeholder="password"
+            value={passwprd}
+            onChange={(event) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+          <button onClick={onClose}>Close</button>
+        </form>
 
     </div>
   )
