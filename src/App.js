c0b5cd15078f771c7b28fbdf59b3d06907e79356
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-04 00:38:13 -0700

    Edit call

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ const App = () => {
 
   const navigate = useNavigate();
 
-
-
     const handleFormSubmit = () => {
       console.log('Selected Category:', selectedCategory);
       console.log('Selected Tech Parameter:', selectedTechParam);
