be0594d83d5125ef0cd4c486dec0fca1d801e9d7
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-03 21:42:44 -0700

    Add bad routes page with some styling to render on DOM

--- src/components/Error.js
+++ src/components/Error.js
@@ -7,7 +7,7 @@ function Error ( {errorRoute, messageRoute} ) {
         <img
           className='error-bad-route-image'
           alt='back home picture with branches'
-          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlW9otdB44JzqHSBZjKkq7wTC8AGeI2exRoPuqymU1OvzAzA9KZB2jdf1SZwIS4ufaWzU&usqp=CAU'
+          src='https://www.ucdenver.edu/images/librariesprovider38/default-album/people-w-disabilities-sized.jpg?sfvrsn=c3a5c3ba_0'
         />
         <h1 className='error-bad-route-message'>ERROR!</h1>
         {errorRoute && <h3 className='error-bad-route'>{errorRoute}</h3>}
