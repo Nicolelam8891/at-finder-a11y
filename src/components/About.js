343187e2d51828ba9b320b8f4be631751380d909
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-03 21:21:48 -0700

    Add About page rendering info and pictures with mission statement

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,23 @@
 import contributors from "../mockData/aboutData";
-import React from 'react';
+import React from "react";
 import "./About.css";
 import Contributor from "./Contributor";
-
 
 function About() {
   return (
     <div className='about-page'>
+      <div className='contributor-card-container'>
+      <div className="mission-statement-container">
+        <h2>Mission Statement</h2>
+        <p className='mission-statement'>
+          Provide an AI-driven application to match the needs of people with
+          disabilities to current and available assistive technologies.
+        </p>
+      </div>
       {contributors.map((contributor) => (
         <Contributor key={contributor.id} {...contributor} />
       ))}
+      </div>
     </div>
   );
 }
