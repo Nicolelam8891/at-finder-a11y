3b35eff05bb71ebd6ac2baae5ffaa0dce2c80758
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-04 16:55:12 -0700

    Add divs and classnames

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -4,6 +4,7 @@ import TechCard from './TechCard'
 function TechList ({tech}) {
   return (
     <div>
+      <h1>Tech List</h1>
       {tech.map((techPiece) =>
         <TechCard 
           key={techPiece.title}
