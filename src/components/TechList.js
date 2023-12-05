9030f7bb021ad3f3d3e72cab25bc234aebc49bb1
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-04 19:00:12 -0700

    Pass props through Results

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,7 +1,7 @@
 import './TechList.css'
 import TechCard from './TechCard'
 
-function TechList ({tech}) {
+function TechList ({tech, onCommentSubmit}) {
   return (
     <div>
       <h1>Tech List</h1>
@@ -9,6 +9,7 @@ function TechList ({tech}) {
         <TechCard 
           key={techPiece.title}
           title={techPiece.title}
+          onCommentSubmit={onCommentSubmit} 
         />
       )
 
