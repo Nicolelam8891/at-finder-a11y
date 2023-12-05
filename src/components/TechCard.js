32d0c981e5091c4ec134a6f492b1eb74cc404712
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-04 19:00:05 -0700

    Pass function info up from Card

--- src/components/TechCard.js
+++ src/components/TechCard.js
@@ -1,11 +1,29 @@
-import './TechCard.css'
+import React, { useState } from 'react';
+import './TechCard.css';
+import CommentForm from './CommentForm'; // Updated import statement
 
-function TechCard ({title}) {
+function TechCard({ title, onCommentSubmit }) {
+  const [showCommentForm, setShowCommentForm] = useState(false);
+
+  const handleTechClick = () => {
+    setShowCommentForm(true);
+  };
+
+  const handleCommentSubmit = (commentData) => {
+    // Pass the commentData up to the parent component
+    onCommentSubmit(commentData);
+    // Optionally, hide the CommentForm after submission
+    setShowCommentForm(false);
+  };
+
+
+
   return (
     <div>
-      <h3>{title}</h3>
+      <h3 onClick={handleTechClick}>{title}</h3>
+      {showCommentForm && <CommentForm title={title} onCommentSubmit={handleCommentSubmit}/>} {/* Updated component name */}
     </div>
-  )
+  );
 }
 
-export default TechCard
+export default TechCard;
