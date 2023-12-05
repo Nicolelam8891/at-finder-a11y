6969ef02278f30041e0c67ef26aabfa8cd0bf23a
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-04 18:59:41 -0700

    Pass props through Results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,24 @@
 import './Results.css'
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, Navigate } from 'react-router-dom';
 import TechList from './TechList';
 import CommentSection from './CommentSection';
+// import comments from '../mockData/comments';
 
-function Results({ tech, comments }) {
-  // const navigate = useNavigate();
+function Results({ tech, techComments }) {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    // Load comments from some source, e.g., an API or local storage
+    // For demonstration purposes, comments are set to an empty array initially
+    setComments(techComments);
+  }, []);
+
+  const handleCommentSubmit = (commentData) => {
+    // Handle the comment submission logic, e.g., send to an API
+    // For demonstration purposes, just updating the state
+    setComments((prevComments) => [...prevComments, commentData]);
+  };
 
   if (!tech || !comments) {
     return <Navigate to="/findmyat" />;
@@ -15,7 +28,7 @@ function Results({ tech, comments }) {
   return (
     <div className='Results'>
       <div  className='results-cont'>
-      <TechList tech={tech} />
+      <TechList tech={tech} onCommentSubmit={handleCommentSubmit}/>
       <CommentSection comments={comments} />
       </div>
     </div>
