5d93866ad17e6fd5d08bbffaeb02b1e23b939290
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-07 13:52:18 -0700

    Pass new props

--- src/components/TechCard.js
+++ src/components/TechCard.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './TechCard.css';
 import CommentForm from './CommentForm'; 
 
-function TechCard({ title, techPiece, onCommentSubmit, category }) {
+function TechCard({ title, website, techPiece, onCommentSubmit, category }) {
   const [showCommentForm, setShowCommentForm] = useState(false);
 
   const handleTechClick = () => {
@@ -19,7 +19,7 @@ function TechCard({ title, techPiece, onCommentSubmit, category }) {
   return (
     <div>
       <h3 onClick={handleTechClick}>{techPiece.title}</h3>
-      <p>{techPiece.link}</p>
+      <p>{website}</p>
       <p>{techPiece.description}</p>
       {showCommentForm && <CommentForm techPiece={techPiece} onCommentSubmit={handleCommentSubmit} category={category}/>} 
     </div>
