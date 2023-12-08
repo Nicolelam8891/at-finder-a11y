937e4b381be8438a021a84b29dd2afd94f127c8c
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-08 11:17:03 -0700

    Add useeffects to empty state

--- src/components/Form.js
+++ src/components/Form.js
@@ -99,6 +99,10 @@ const Form = ({
     };
   }, []);
 
+  useEffect(()=>{
+    setSelectedCategory(null)
+  }, [])
+
   return (
     <div className='Form'>
       <h1>Choose a category</h1>
@@ -130,7 +134,7 @@ const Form = ({
         )}
       </div>
       {loading ? (
-        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg className='loader' width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <rect className="spinner_9y7u" x="1" y="1" rx="1" width="10" height="10"/>
           <rect className="spinner_9y7u spinner_DF2s" x="1" y="1" rx="1" width="10" height="10"/>
           <rect className="spinner_9y7u spinner_q27e" x="1" y="1" rx="1" width="10" height="10"/>
