911aa126c8a7f31066c50fb911d4a56b052159be
Jan McSorley <mcsorleyjan@gmail.com>
2023-12-07 19:59:13 -0600

    Add loading and error to the LOAD_COMMENTS query

--- src/components/TestComments/TestComments.js
+++ src/components/TestComments/TestComments.js
@@ -6,10 +6,26 @@ function TestComments() {
     const { loading, error, data } = useQuery(LOAD_COMMENTS);
     console.log("DATA", data)
 
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>Error: {error.message}</p>;
+    }
+
+    const commentIds = data.allCategoryComments.map((comment) => {
+        return (
+            <p>{comment.id}</p>
+        )
+    })
+
     return (
         <div>
             <h2>Hello from TestComments</h2>
-            
+            <div>
+                {commentIds}
+            </div>
         </div>
     )
 }
