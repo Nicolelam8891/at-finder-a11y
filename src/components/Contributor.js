f2cb5ead5344ca63fe8555b491a463c445bccf64
Jan McSorley <mcsorleyjan@gmail.com>
2023-12-10 15:55:23 -0600

    Add styling to About page

--- src/components/Contributor.js
+++ src/components/Contributor.js
@@ -3,7 +3,7 @@ import './Contributor.css'
 function Contributor({ name, role, bio, imageUrl }) {
   return (
     <div className="contributor-card">
-      <img src={imageUrl} alt={`Photo of ${name}`} className="contributor-image" />
+      <img src={imageUrl} alt={`Headshot of ${name}`} className="contributor-image" />
       <h3>{name}</h3>
       <p>{role}</p>
       <p>{bio}</p>
