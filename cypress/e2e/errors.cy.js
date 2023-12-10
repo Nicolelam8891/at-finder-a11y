c4a5195b5d7c0465252312a55f465e44374d9378
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-10 14:44:52 -0700

    Add minor chages to errors

--- cypress/e2e/errors.cy.js
+++ cypress/e2e/errors.cy.js
@@ -1,9 +1,9 @@
 describe('template spec', () => {
   it("Tests a bad route page for the team page", () => {
-    cy.visit("http://localhost:3000/nonsense");
+    cy.visit("http://localhost:3000/nonsense")
     cy.get('.error-bad-route-container').get('.error-bad-route-image').should("have.attr", "src").should("eq", "https://www.inclusionhub.com/hubfs/Blog/disabled-people-in-park-setting-1.jpg")
     cy.get('.error-bad-route-message').contains("h1", "ERROR")
     cy.get('.error-bad-route').contains("h3", "Oh no, you have gone down the wrong path. Find your path back home by clicking on the home button!" )
-    cy.get('.header').get('.logo').click().url().should("eq", "http://localhost:3000/")
+    cy.get('.header').get('.logo').click().url().should("eq", "http://localhost:3000/");
   });
 })
