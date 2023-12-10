32551d4cfa052dec51615a014b906a3bfb673a7a
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-10 13:35:44 -0700

    Finished pictures & bio for all group members

--- src/mockData/aboutData.js
+++ src/mockData/aboutData.js
@@ -2,8 +2,8 @@ import nicoleImage from '../assets/nicole.png';
 import janImage from '../assets/jan.png';
 import elizaImage from '../assets/eliza.png';
 import joopImage from '../assets/joop.png';
-// import nickImage from '../assets/nick.png';
-// import jackImage from '../assets/jack.png';
+import nickImage from '../assets/nick.png';
+import jackImage from '../assets/jack.png';
 
 const contributors = [
   {
@@ -12,7 +12,7 @@ const contributors = [
     role: 'Front End Software Developer',
     bio: 'Nicole is a software engineer and registered nurse at at a hospital serving the underprivledged populations in Denver, CO. Driven by a passion for organization, attention-to-detail, and creativity, she transitioned herself into Tech through Turing Front End Engineering Program. Nicole aims to expand her tech skills into making impactful work, striving to make a difference and assist people in underserved populations. Her short-term goal is to learn Typescript and Redux. Her long-term goal is to become a full-stack Software Developer or Projecr Manager.', 
     imageUrl: nicoleImage,
-   
+    altText: "Nicole Lam headshot"
   },
   {
     id: 2,
@@ -20,6 +20,7 @@ const contributors = [
     role: 'Front End Software Developer',
     bio: 'Jan is passionate about the importance of inclusive design and accessibility. She has worked in education and corporate settings has witnessed the difference inclusion can make and how it leads to more innovative solutions. Jan came to Turing to better understand the developer experience and to identify natural touch points in the Software Development Life Cycle where accessibility can be effectively integrated. After Turing, she hopes to be a part of eliminating barriers to access for people with disabilities by helping bridge the knowledge gap that leads to inaccessible digital tools.',
     imageUrl: janImage,
+    altText: "Jan McSorley headshot"
    
   },
   {
@@ -27,23 +28,25 @@ const contributors = [
     name: 'Jack Alfonso',
     role: 'Front End Software Developer',
     bio: 'Jack is an avid reader, sports fan, and swimmer. His professional background is in sales and education, and he has taken the skills he developed in those professions into his work as a frontend developer. Whether it is in heavily collaborative environments or more individualized coding work, Jack/s ability to communicate and organize effectively has allowed him to efficiently develop websites of various scopes. He is interested in building engaging applications with intuitive UI/UX that help people connect and learn.',
-    // imageUrl: jackImage
-   
+    imageUrl: jackImage,
+    altText: "Jack Alfonso headshot"
   },
   {
     id: 4,
     name: 'Joop Stark',
     role: 'Back End Software Developer',
     bio: 'Joop has been trying to save the world. First a Recon Marine, then a Special Education Teacher, and now a Back End Software Engineer. What he really wants is to work for a company making a positive social or environmental impact that gives him problems to solve.',
-    imageUrl: joopImage
-   
+    imageUrl: joopImage,
+    altText: "Joop Stark headshot"
+
   },
   {
     id: 5,
     name: 'Nicholas Spencer',
     role: 'Back End Software Developer',
-    bio: 'Hello, I/m Nick Spencer, a resilient individual with a passion for software development. My journey has been anything but conventional — overcoming a cancer diagnosis and navigating diverse roles from phlebotomy to custodial management. Now, on the verge of graduating from the Turing School of Software and Design, I/m equipped with a robust skill set and a unique perspective. My dedication to continuous learning, problem-solving, and leadership, combined with a love for coding, drives me to create innovative solutions. Join me on this exciting journey as I embark on a new chapter, bringing determination, adaptability, and a fresh outlook to the dynamic world of software development.',
-    // imageUrl: nickImage
+    bio: 'Nick is resilient and has a passion for software development. His journey has been anything but conventional — overcoming a cancer diagnosis and navigating diverse roles from phlebotomy to custodial management. Now, on the verge of graduating from the Turing School of Software and Design, Nick is equipped with a robust skill set and a unique perspective. His dedication to continuous learning, problem-solving, and leadership, combined with a love for coding, drives him to create innovative solutions. Join him on this exciting journey as he embarks on a new chapter, bringing determination, adaptability, and a fresh outlook to the dynamic world of software development.',
+    imageUrl: nickImage,
+    altText: "Nicholas Spencer headshot"
    
   },
   {
@@ -52,6 +55,8 @@ const contributors = [
     role: 'Back End Software Developer',
     bio: 'Eliza is a software developer from Denver, CO. After finishing college with a focus on music she decided to make a large leap into learning about back-end development through the Back End program at Turing School of Software and Design. Her love for constant learning drives her strong passion towards solving problems to help people. She can/t wait to continue her journey with software developer and hopes to someday work in the video game industry and create games many people can enjoy.',
     imageUrl: elizaImage,
+    altText: "Eliza Keating headshot"
+    
   },
 ];
 
