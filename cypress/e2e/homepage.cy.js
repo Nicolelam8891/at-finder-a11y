643a414f8ee11ece2d374364594f8daa6cabf256
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-09 20:45:53 -0700

    Cypress testing for homepage and about page complete

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,15 +1,55 @@
 describe('Homepage user flow', () => {
   beforeEach(() => {
+     // Intercept POST requests to the GraphQL endpoint
     cy.intercept(
       "POST", 
-      "https://nameless-stream-88171-cdff591c89ed.herokuapp.com/graphql"
-      {
-        statusCode: 200,
-        fixture: "users",
+      "https://nameless-stream-88171-cdff591c89ed.herokuapp.com/graphql", (req) => {
+        if (req.body && req.body.operationName === 'SessionCreate') {
+          req.reply((res) => {
+           res.send({
+             fixture: 'users.json', // Provide a fixture file with the desired response
+             statusCode: 200,
+           });
+          })
+        }
       }
     ).as("users")
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000/')
   })
-  it('passes', () => {
+  it('it should contain a header, h1, log-in button, about button, two informational blocks, and buttons for additiona information', () => {
+    cy.get('.header').get('.logo').click().url().should("eq", "http://localhost:3000/")
+    cy.get('h1').contains("h1", "AI AT Ally")
+    cy.get('.login-button').contains("Log In")
+    cy.get('.about-button').contains("About")
+    cy.get('.info-container').contains("h2", "Welcome to AI AT Ally")
+    cy.get('.info-container > p').contains('p', "If you need to research assistive")
+    cy.get('.find-AT-button').contains("Find my AT")
+    cy.get('.learn-AT-button').contains("Learn more about AT")
+    cy.get('.AT-info').get('.what-is-at').contains("h2", "What is assistive technology")
+    cy.get('div.info-description > :nth-child(1)').contains("p", "Assistive Technology is anything a person")
+    cy.get('div.info-description > :nth-child(2)').contains("p", "Assistive technology helps people")
   })
+  
+  it('it should allow user to log in', () => {
+    cy.get('.login-button').contains("Log In").click()
+    cy.get('#email').should("have.attr", "placeholder", "email").type("rosa@aol.com").should("have.value", "rosa@aol.com")
+    cy.get('#password').should("have.attr", "placeholder", "password").type("rosaslaw10!").should("have.value", "rosaslaw10!")
+    cy.get('form > button').contains("Login")
+  })
+  
+  it('it should allow user to go to the About page', () => {
+    cy.get('.about-button').contains("About").click()
+    cy.get('.mission-statement-container').get('h2').contains("h2", "Mission Statement")
+    cy.get('.mission-statement').contains("p", "Provide an AI-driven application")
+    cy.get('.contributor-card-container > :nth-child(2)').first().get(':nth-child(2) > .contributor-image').should("have.attr", "src").and('match', /nicole\.\w+\.png$/)
+    cy.get(':nth-child(2) > h3').contains("h3", "Nicole Lam")
+    cy.get('.contributor-card-container > :nth-child(2) > :nth-child(3)').contains("p", "Front End Software Developer")
+    cy.get(':nth-child(2) > :nth-child(4)').contains("p", "Nicole is a software engineer")
+    cy.get('.contributor-card-container > :nth-child(7)').last().get(':nth-child(7) > .contributor-image').should("have.attr", "src").and('match', /eliza\.\w+\.png$/)
+    cy.get(':nth-child(7) > h3').contains("h3", "Eliza Keating")
+    cy.get(':nth-child(7) > :nth-child(3)').contains("p", "Back End Software Developer")
+    cy.get(':nth-child(7) > :nth-child(4)').contains("p", "Eliza is cool")
+  })
+
 })
+
--- src/mockData/aboutData.js
+++ src/mockData/aboutData.js
@@ -10,7 +10,7 @@ const contributors = [
     id: 1,
     name: 'Nicole Lam',
     role: 'Front End Software Developer',
-    bio: 'Nicole is a a software engineer and registered nurse at at a hospital serving the underprivledged populations in Denver, CO. Driven by a passion for organization, attention-to-detail, and creativity, she transitioned herself into Tech through Turing Front End Engineering Program. Nicole aims to expand her tech skills into making impactful work, striving to make a difference and assist people in underserved populations. Her short-term goal is to learn Typescript and Redux. Her long-term goal is to become a full-stack Software Developer or Projecr Manager.', 
+    bio: 'Nicole is a software engineer and registered nurse at at a hospital serving the underprivledged populations in Denver, CO. Driven by a passion for organization, attention-to-detail, and creativity, she transitioned herself into Tech through Turing Front End Engineering Program. Nicole aims to expand her tech skills into making impactful work, striving to make a difference and assist people in underserved populations. Her short-term goal is to learn Typescript and Redux. Her long-term goal is to become a full-stack Software Developer or Projecr Manager.', 
     imageUrl: nicoleImage,
    
   },
