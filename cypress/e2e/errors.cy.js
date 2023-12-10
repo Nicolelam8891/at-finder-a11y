d4e326ae05a95ccae68735c825c7991bc12aac59
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-10 14:05:12 -0700

    Cypress for bad routes page complete

--- cypress/e2e/errors.cy.js
+++ cypress/e2e/errors.cy.js
@@ -1,5 +1,9 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  it("Tests a bad route page for the team page", () => {
+    cy.visit("http://localhost:3000/nonsense");
+    cy.get('.error-bad-route-container').get('.error-bad-route-image').should("have.attr", "src").should("eq", "https://www.ucdenver.edu/images/librariesprovider38/default-album/people-w-disabilities-sized.jpg?sfvrsn=c3a5c3ba_0")
+    cy.get('.error-bad-route-message').contains("h1", "ERROR")
+    cy.get('.error-bad-route').contains("h3", "Oh no, you have gone down the wrong path. Find your path back home by clicking on the home button!" )
+    cy.get('.header').get('.logo').click().url().should("eq", "http://localhost:3000/")
+  });
 })
