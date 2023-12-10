95ef2a61e4cb0e5307b7a428205fa86bebe60e0c
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-12-10 11:44:17 -0700

    Add cypress testing for AT details

--- cypress/e2e/aiTech.cy.js
+++ cypress/e2e/aiTech.cy.js
@@ -1,13 +1,29 @@
 describe('template spec', () => {
   beforeEach(() => {
-    cy.intercept('POST', 'https://assistivie-tech-2307-648a3d563927.herokuapp.com/', {
+    cy.intercept('POST', 'https://assistivie-tech-2307-648a3d563927.herokuapp.com/api/v1/ai_requests', {
       statusCode: 201,
-      fixture: 'aiTechResults',
+      fixture: 'aiTechResults'
     })
       .as('getAiTech')
-      .visit('http://localhost:3000/');
-  })
-  it('it should allow user to go to the Find my AT page', () => {
+
+      cy.intercept('POST', 'https://nameless-stream-88171-cdff591c89ed.herokuapp.com/graphql', (req) => {
+    req.reply({
+      statusCode: 201,
+      fixture: 'getAllComments, createComment' 
+    });
+  }).as('graphqlRequest');
+      cy.visit('http://localhost:3000/findmyat');
+    });
    
+    
+      it('should allow the user to get details about assistive technologies ', () => {
+    
+          cy.get('.category-list > :nth-child(1)').click();
+          cy.get('.subcategory-list > :nth-child(1)').contains("Screen Readers").click();
+          cy.get('.submit').contains("Submit").click()
+          cy.wait('@getAiTech');
+          cy.wait('@graphqlRequest');
+        })
   });
-})
+   
+
