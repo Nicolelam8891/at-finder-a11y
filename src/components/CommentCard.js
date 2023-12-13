c73dc2f2f78663e2b73c5cc3dfbebd8a598944f7
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-12 17:39:07 -0700

    Add icon as background

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -2,10 +2,13 @@ import './CommentCard.css'
 
 function CommentCard ({comment}) {
   return (
-    <div className={comment.rating === true ? 'liked-comment commentcard' : 'disliked-comment commentcard'}>
+    <div className='container'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={comment.rating === true ? 'liked-comment commentcard' : 'disliked-comment commentcard'}><path className="primary" d="M2 15V5c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v15a1 1 0 0 1-1.7.7L16.58 17H4a2 2 0 0 1-2-2z"/><path className="secondary" d="M6 7h12a1 1 0 0 1 0 2H6a1 1 0 1 1 0-2zm0 4h8a1 1 0 0 1 0 2H6a1 1 0 0 1 0-2z"/></svg>
+      <div className='containerText'>
       <p>{comment.category}</p>
       <p>{comment.title}</p>
       <p>{comment.userComment}</p>
+      </div>
     </div>
   )
 }
