31fa05441ded6c1c18095b1ea98d7ff908b06252
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2024-01-13 14:17:34 -0700

    Add CSS changes to About page

--- src/components/Contributor.js
+++ src/components/Contributor.js
@@ -5,8 +5,10 @@ function Contributor({ name, role, bio, imageUrl }) {
     <div className="contributor-card">
       <img src={imageUrl} alt={`Headshot of ${name}`} className="contributor-image" />
       <h3>{name}</h3>
-      <p>{role}</p>
-      <p>{bio}</p>
+        <div className='contributor-card-info'>
+          <p><em>{role}</em></p>
+          <p>{bio}</p>
+        </div>
     </div>
   );
 }
