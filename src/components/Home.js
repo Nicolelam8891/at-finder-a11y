22f856270f9165b32547dd180ffca343dbb28ea3
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2024-01-17 22:13:06 -0700

    Add images to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import "./Home.css";
+import Finding_AT from '../assets/Finding_AT.png';
+import RobotBook from '../assets/RobotBook.png'
 
 function Home( {handleButtonClick} ) {
   return (
@@ -8,10 +10,11 @@ function Home( {handleButtonClick} ) {
       <div className='info-container'>
         <p className="main-page-text">Welcome to AI AT Ally! If you need to research assistive technologies (AT) for yourself or a loved one, you've come to the right place. AI AT Ally, uses generative artificial intelligence (AI) to find tools that support the needs of people with disabilities. Please explore the site to learn more, leave comments, or rate tools you may have experience using.</p>
         <div className='splash-button-container'>
-          <button className='find-AT-button' onClick={() => handleButtonClick('/findmyat')}>Find my AT
-          <p>Click here to select the technology of your choice</p></button>
-          <button className='learn-AT-button' onClick={() => handleButtonClick('/learnat')}>Learn more
-          <p>Click here to learn more about assistive technology!</p>
+          <button className='find-AT-button' onClick={() => handleButtonClick('/findmyat')}>Select your assistive technology!
+          <img src={Finding_AT} alt="Robot-AT-hand" className="AT-magnify-image"></img>
+          </button>
+          <button className='learn-AT-button' onClick={() => handleButtonClick('/learnat')}>Select to learn about assistive technology!
+          <img src={RobotBook} alt="Robot-AT-hand" className="robot-learn-more-image"></img>
           </button>
          
         </div>
