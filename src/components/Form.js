c59f34d36b488beb290b0485133b4164483af29d
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2024-03-11 12:22:51 -0600

    Add changes to center image and move cards to right

--- src/components/Form.js
+++ src/components/Form.js
@@ -44,11 +44,6 @@ const Form = ({
     }
   }, [urlSelectedCategory])
 
-  // const scrollToBottom = () => {
-  //   if (bottomRef.current) {
-  //     window.scrollTo({ top: bottomRef.current.offsetTop, behavior: 'smooth' });
-  //   }
-  // };
 
   const handleCategoryClick = (category) => {
     if (selectedCategory === category) {
@@ -62,23 +57,6 @@ const Form = ({
     }
     setSelectedTechParam(null);
   };
-
-  // const handleTechParamClick = (techParam) => {
-  //   setSelectedTechParam(techParam);
-  //   setTimeout(scrollToBottom, 2) (NOTE REMOVE THIS NO MATTER WHAT)
-  // };
-
-  // const handleTechParamClick = async (techParam) => {
-  //   setSelectedTechParam(techParam);
-  //   try {
-  //     setLoading(true);
-  //     await onFormSubmit();
-  //   } catch (error) {
-  //     console.error('Error', error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
 
   const handleTechParamClick = async (techParam, category) => {
     setSelectedTechParam(techParam);
@@ -130,10 +108,15 @@ return (
     <div className={selectedCategory ? 'singled' : "category-list"}>
     {/* <div className={selectedCategory ? 'singled' : "category-list"} ref={containerRef}> */}
     {selectedCategory ? (
-        <div>
+
+        <div className='category'>
+          <div className='category-all'> 
           <h2 className='sub'>{`Tools for ${selectedCategory}`}</h2>
-          <img src={categoryImage} alt="" />
-          <p>Choose a tool category to have ChatGPT generate a list of assistive technologies.</p>
+          <img src={categoryImage} alt="" className='category-image' />
+          <p className='category-text'>Choose a tool category to have ChatGPT generate a list of assistive technologies.</p>
+          </div>
+        <div/>
+
           <div className='subcategory-list'>
           {getTechParams().map((tech, index) => {
              const category = selectedCategory;  // Define category here
@@ -150,20 +133,6 @@ return (
                   </div>
                 );
             })}
-            {/* {getTechParams().map((tech, index) => (
-              <div
-                tabIndex="0"
-                key={index}
-                className={tech === selectedTechParam ? 'selected techParam' : 'techParam'}
-                onClick={() => handleTechParamClick(tech)}
-                onKeyDown={(e) => handleTechKeyDown(e, tech)}
-                role="button"
-              >
-                <p>
-                  {tech}
-                </p>
-              </div>
-            ))} */}
           </div>
         </div>
       ) : (
@@ -180,8 +149,8 @@ return (
       )}
     </div>
     {loading && (
-      <div>
-      <p>Please wait while ChatGPT fetches your requested assistive technologies.</p>
+      <div className='chat-div'>
+      <p className='chat-message'>Please wait while ChatGPT fetches your requested assistive technologies.</p>
       <svg className='loader' width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <rect className="spinner_9y7u" x="1" y="1" rx="1" width="10" height="10"/>
         <rect className="spinner_9y7u spinner_DF2s" x="1" y="1" rx="1" width="10" height="10"/>
